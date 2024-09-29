2e6ec911411ddaacaa341c24a71beac19a9b642b
MShubar <pokeeeeepow@gmail.com>
2024-09-29 13:00:52 +0300

    last commit

--- js/app.js
+++ js/app.js
@@ -1,50 +1,123 @@
 /*-------------------------------- Constants --------------------------------*/
+// these are the combinations used to check if I won
+const winningCombos = [
+  [0, 1, 2],
+  [0, 4, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 4, 6],
+  [2, 5, 8],
+  [3, 4, 5],
+  [6, 7, 8]
+]
+
 /*---------------------------- Variables (state) ----------------------------*/
-//this represents the array squares
+//the squares are considered an array board[0] to board[8]
 let board = []
-//the starting turn is X as always
-let turn = ["X", "O"]
-// who won out of the 2
+//the turn starts with X later update in functions
+let turn = '🚀'
+//to be changed to true later if someone wins
 let winner = false
-// if it is a tie or not
 let tie = false
 
 /*------------------------ Cached Element References ------------------------*/
-//squares and messages
-const squareEls = document.querySelector(".sqr")
-const messageEl = document.querySelector("#message")
+const squareEls = document.querySelectorAll('.sqr')
+const messageEl = document.querySelector('#message')
+const resetBtnEl = document.getElementById('reset')
 
-// /*-------------------------------- Functions --------------------------------*/
+/*-------------------------------- Functions --------------------------------*/
 
-const EmptySqureIndex = (event) => {
-  const squareId = event.target.id
-  board.push(squareId)
-  console.log(board)
+const handleClick = (event) => {
+  const squareIndex = event.target.id
+  if (board[squareIndex] !== '' || winner) return
+
+  placePiece(squareIndex)
+  checkForWinner()
+  checkForTie()
+  switchPlayerTurn()
+  render()
 }
 
-const switchPlayer = () => {
-  turn = turn === "X" ? "O" : "X"
+const placePiece = (index) => {
+  board[index] = turn
 }
-const render = () => {}
+
+const checkForWinner = () => {
+  for (let i = 0; i < winningCombos.length; i++) {
+    let combo = winningCombos[i]
+    if (
+      board[combo[0]] !== '' &&
+      board[combo[0]] === board[combo[1]] &&
+      board[combo[0]] === board[combo[2]]
+    ) {
+      winner = true
+      break
+    }
+  }
+}
+
+const checkForTie = () => {
+  if (winner) return
+
+  tie = true
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === '') {
+      tie = false
+      break
+    }
+  }
+}
+
+const switchPlayerTurn = () => {
+  if (winner) {
+    return
+  }
+  if (turn === '🚀') {
+    turn = '👽'
+  } else {
+    turn = '🚀'
+  }
+}
+
 const updateBoard = () => {
-  board.forEach((button) => {
-    button.addEventListener("click", () => {
-      const buttonValue = button.textContent
-    })
+  squareEls.forEach((square, index) => {
+    square.textContent = board[index]
   })
 }
-const handleBoxClick = (event) => {
-  event.preventDefault()
-  const box = even.target
-  if(box.classList.contains())
+
+const updateMessage = () => {
+  if (winner) {
+    messageEl.textContent = `${turn} wins!`
+  } else if (tie) {
+    messageEl.textContent = `It's a tie!`
+  } else {
+    messageEl.textContent = `It's ${turn}'s turn.`
+  }
 }
-const init = (event) => {
-  EmptySqureIndex(event)
-  switchPlayer()
+
+const render = () => {
   updateBoard()
+  updateMessage()
+}
+
+const init = () => {
+  //the squares are considered an array board[0] to board[8]
+  board = ['', '', '', '', '', '', '', '', '']
+  //the turn starts with 🚀 later update in functions
+  turn = '🚀'
+  //to be changed to true later if someone wins
+  winner = false
+  //to be changed to true later if it is a tie
+  tie = false
+  //
+  render()
 }
 
 /*----------------------------- Event Listeners -----------------------------*/
-document.querySelectorAll(".sqr").forEach((div) => {
-  div.addEventListener("click", init)
+squareEls.forEach((square) => {
+  square.addEventListener('click', handleClick)
 })
+
+resetBtnEl.addEventListener('click', init)
+
+init()
